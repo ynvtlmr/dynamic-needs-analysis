25222d1424c5166e6f4174f7107c12dbe72bd339
Yaniv Talmor <ynvtlmr@gmail.com>
2023-12-04 16:39:02 -0800

    add download and upload buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,41 @@ import { BeneficiaryComponent } from "./beneficiary/beneficiary.component";
 })
 export class AppComponent {
   title = 'dynamic-needs-analysis';
+
   clearAllLocalStorage() {
     localStorage.clear();
     window.location.reload();
+  }
+
+  downloadLocalStorageAsFile() {
+    const data = JSON.stringify(localStorage);
+    const blob = new Blob([data], { type: 'application/json' });
+    const fileName = 'dna-local-storage.json';
+    const downloadURL = window.URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = downloadURL;
+    link.download = fileName;
+    link.click();
+
+    // Revoke the object URL to free up memory
+    window.URL.revokeObjectURL(downloadURL);
+  }
+
+  loadLocalStorageFromFile(event: Event) {
+    const file = (event.target as HTMLInputElement)?.files?.[0];
+    if(!file) {
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      const data = JSON.parse(reader.result as string);
+      for(let key in data) {
+        localStorage.setItem(key, data[key]);
+      }
+      // Reload app
+      window.location.reload();
+    };
+    reader.readAsText(file);
   }
 }
