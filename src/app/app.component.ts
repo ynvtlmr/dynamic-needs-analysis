1bc665c28b2722013891a1bc8f568a4aba939ec9
Yaniv Talmor <ynvtlmr@gmail.com>
2023-12-08 09:47:46 -0800

    change nav to use buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ClientComponent } from './client/client.component';
 import { BeneficiaryComponent } from './beneficiary/beneficiary.component';
 import { BusinessComponent } from './business/business.component';
 import { GoalComponent } from './goal/goal.component';
 import { LocalStorageService } from './services/local-storage.service';
 import { AssetComponent } from './finance/asset.component';
+
+interface NavLink {
+  path: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -29,7 +34,7 @@ export class AppComponent {
   title = 'dynamic-needs-analysis';
   selectedTab: string = 'client';
 
-  constructor(private localStorageService: LocalStorageService) {}
+  constructor(private localStorageService: LocalStorageService, private router: Router) {}
 
   clearAllLocalStorage() {
     this.localStorageService.clearAll();
@@ -42,4 +47,13 @@ export class AppComponent {
   loadLocalStorageFromFile(event: Event) {
     this.localStorageService.loadFromFile(event);
   }
+
+  navLinks: NavLink[] = [
+    { path: 'client', label: 'Client' },
+    { path: 'beneficiaries', label: 'Beneficiaries' },
+    { path: 'business', label: 'Business' },
+    { path: 'goal', label: 'Goal' },
+    { path: 'asset', label: 'Asset' },
+    { path: 'debt', label: 'Debt' },
+  ];
 }
