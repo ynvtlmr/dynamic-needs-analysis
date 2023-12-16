860bd855f8d31bb083e9b0e36adf5e43f2aa3b33
Yaniv Talmor <ynvtlmr@gmail.com>
2023-12-16 00:55:00 -0800

    change diversification to use apex-charts too

--- src/app/outputs/diversification/diversification.component.ts
+++ src/app/outputs/diversification/diversification.component.ts
@@ -1,37 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart,
+  ApexLegend,
+  NgApexchartsModule,
+} from 'ng-apexcharts';
 import { Asset } from '../../inputs/asset/asset.component';
 import { LocalStorageService } from '../../services/local-storage.service';
-import { colorSets, Color, NgxChartsModule } from '@swimlane/ngx-charts';
+
+export type PieChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: string[];
+  legend: ApexLegend;
+};
 
 @Component({
   selector: 'app-diversification',
   templateUrl: './diversification.component.html',
-  imports: [NgxChartsModule],
+  imports: [NgApexchartsModule],
   standalone: true,
 })
 export class DiversificationComponent implements OnInit {
-  data: any[] = [];
-  colorScheme: Color = colorSets.find((s) => s.name === 'cool') || colorSets[0];
+  public chartOptions: PieChartOptions;
 
-  constructor(private localStorageService: LocalStorageService) {}
-
-  ngOnInit(): void {
-    this.processData();
+  constructor(private localStorageService: LocalStorageService) {
+    // Initialize PieChartOptions with default values
+    this.chartOptions = {
+      series: [],
+      chart: { type: 'pie' },
+      responsive: [{ breakpoint: 400, options: { chart: { width: 200 } } }],
+      labels: [],
+      legend: { position: 'bottom' },
+    };
   }
 
-  processData(): void {
-    const assets: Asset[] = this.localStorageService.getItem('assets') || [];
-    const totalByType: Record<string, number> = assets.reduce(
-      (acc: Record<string, number>, asset: Asset) => {
-        acc[asset.type] = (acc[asset.type] || 0) + asset.currentValue;
-        return acc;
-      },
-      {},
-    );
+  ngOnInit(): void {
+    this.prepareChartData();
+  }
 
-    this.data = Object.keys(totalByType).map((type) => ({
-      name: type,
-      value: totalByType[type],
-    }));
+  private prepareChartData(): void {
+    const assets: Asset[] = this.localStorageService.getItem('assets') || [];
+    const totalByType: Record<string, number> = {};
+
+    assets.forEach((asset) => {
+      totalByType[asset.type] =
+        (totalByType[asset.type] || 0) + asset.currentValue;
+    });
+
+    this.chartOptions.series = Object.values(totalByType);
+    this.chartOptions.labels = Object.keys(totalByType);
   }
 }
