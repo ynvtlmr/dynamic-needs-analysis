f8254d39be30ea83dd7d54cb731808ec7ed022ef
Yaniv Talmor <ynvtlmr@gmail.com>
2024-01-05 09:59:14 -0800

    register service worker for PWA

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// check if the service worker is enabled and then register it.
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('ngsw-worker.js', { scope: '/' });
+}
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
