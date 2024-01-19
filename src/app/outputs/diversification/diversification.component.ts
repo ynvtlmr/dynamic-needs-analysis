6582ecfafeaaa57ff5f21089ca42e5628d7229b4
Yaniv Talmor <ynvtlmr@gmail.com>
2024-01-18 20:51:21 -0800

    add more colours to diversification output

--- src/app/outputs/diversification/diversification.component.ts
+++ src/app/outputs/diversification/diversification.component.ts
@@ -10,12 +10,26 @@ import {
 import { Asset } from '../../models/asset.model';
 import { LocalStorageService } from '../../services/local-storage.service';
 
+const CUSTOM_COLORS: string[] = [
+  '#1f77b4',
+  '#ff7f0e',
+  '#2ca02c',
+  '#d62728',
+  '#9467bd',
+  '#8c564b',
+  '#e377c2',
+  '#7f7f7f',
+  '#bcbd22',
+  '#17becf',
+];
+
 export type PieChartOptions = {
   series: ApexNonAxisChartSeries;
   chart: ApexChart;
   responsive: ApexResponsive[];
   labels: string[];
   legend: ApexLegend;
+  colors: string[];
 };
 
 @Component({
@@ -40,6 +54,7 @@ export class DiversificationComponent implements OnInit, OnDestroy {
       responsive: [{ breakpoint: 400, options: { chart: { width: 200 } } }],
       labels: [],
       legend: { position: 'bottom' },
+      colors: CUSTOM_COLORS,
     };
   }
 
