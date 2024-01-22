6296512fb616dbe23fd147ae83351c0c9fd7169b
Yaniv Talmor <ynvtlmr@gmail.com>
2024-01-21 16:00:07 -0800

    add priority slider and coverage to total insurable needs

--- src/app/outputs/total-insurable-needs/total-insurable-needs.component.ts
+++ src/app/outputs/total-insurable-needs/total-insurable-needs.component.ts
@@ -2,23 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { Asset } from '../../models/asset.model';
 import { CurrencyPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-total-insurable-needs',
   templateUrl: './total-insurable-needs.component.html',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, FormsModule],
   standalone: true,
 })
 export class TotalInsurableNeedsComponent implements OnInit {
   estateTaxLiability: number = 0;
+  percentageAllocation: number = 0;
+  desiredInsuranceCoverage: number = 0;
 
   constructor(private localStorageService: LocalStorageService) {}
 
   ngOnInit(): void {
-    // Fetch assets from LocalStorageService
     const assets: Asset[] =
       this.localStorageService.getItem<Asset[]>('assets') ?? [];
     this.estateTaxLiability = this.calculateEstateTaxLiability(assets);
+
+    const liabilityAllocations =
+      this.localStorageService.getItem<{ [key: string]: number }>(
+        'liabilityAllocations',
+      ) ?? {};
+    this.percentageAllocation = liabilityAllocations['estateTaxLiability'] ?? 0;
+    this.calculateDesiredInsuranceCoverage();
   }
 
   private calculateEstateTaxLiability(assets: Asset[]): number {
@@ -34,6 +43,28 @@ export class TotalInsurableNeedsComponent implements OnInit {
       asset.currentValue * Math.pow(1 + asset.rate / 100, asset.term);
     return (
       (futureValue - asset.initialValue) * (asset.capitalGainsTaxRate / 100.0)
+    );
+  }
+
+  onPercentageChange(): void {
+    this.calculateDesiredInsuranceCoverage();
+    this.storePercentageAllocation();
+  }
+
+  private calculateDesiredInsuranceCoverage(): void {
+    this.desiredInsuranceCoverage =
+      this.estateTaxLiability * (this.percentageAllocation / 100);
+  }
+
+  private storePercentageAllocation(): void {
+    const liabilityAllocations =
+      this.localStorageService.getItem<{ [key: string]: number }>(
+        'liabilityAllocations',
+      ) ?? {};
+    liabilityAllocations['estateTaxLiability'] = this.percentageAllocation;
+    this.localStorageService.setItem(
+      'liabilityAllocations',
+      liabilityAllocations,
     );
   }
 }
